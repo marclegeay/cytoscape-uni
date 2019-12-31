30d23c639c28549b9c68e6cf93324d427ea8e841
Marc Legeay <marc.legeay@cpr.ku.dk>
2019-12-31 03:03:50 +0100

    Version 1.0

---
+++ src/main/java/dk/ku/cpr/UNI/internal/CyActivator.java
@@ -0,0 +1,16 @@
+package dk.ku.cpr.UNI.internal;
+
+import org.cytoscape.model.events.AddedNodesListener;
+import org.cytoscape.service.util.AbstractCyActivator;
+import org.cytoscape.session.events.SessionLoadedListener;
+import org.osgi.framework.BundleContext;
+
+public class CyActivator extends AbstractCyActivator {
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+		UUIDManager manager = new UUIDManager();
+		registerService(context, manager, AddedNodesListener.class);
+		registerService(context, manager, SessionLoadedListener.class);
+	}
+}
---
+++ src/main/java/dk/ku/cpr/UNI/internal/UUIDManager.java
@@ -0,0 +1,70 @@
+package dk.ku.cpr.UNI.internal;
+
+import org.cytoscape.model.CyNetwork;
+import org.cytoscape.model.CyNode;
+import org.cytoscape.model.CyRow;
+import org.cytoscape.model.CyTable;
+import org.cytoscape.model.events.AddedNodesEvent;
+import org.cytoscape.model.events.AddedNodesListener;
+import org.cytoscape.session.events.SessionLoadedEvent;
+import org.cytoscape.session.events.SessionLoadedListener;
+
+public class UUIDManager implements AddedNodesListener, SessionLoadedListener {
+	
+	private static final String NAMESPACE = "UNI";
+	private static final String COLUMN_NAME = "UUID";
+	private static long nextUUID=1;
+
+	@Override
+	public void handleEvent(AddedNodesEvent e) {
+		CyTable nodeTable = e.getSource().getDefaultNodeTable();
+		
+		if(nodeTable.getColumn(NAMESPACE, COLUMN_NAME) == null) {
+			nodeTable.createColumn(NAMESPACE, COLUMN_NAME, Long.class, true);
+		}
+		
+		for(CyNode cyNode : e.getPayloadCollection()) {
+			CyRow nodeRow = nodeTable.getRow(cyNode.getSUID());
+			
+			if(nodeRow.get(NAMESPACE, COLUMN_NAME, Long.class) == null) {
+				nodeRow.set(NAMESPACE, COLUMN_NAME, nextUUID++);
+			}
+		}
+	}
+
+	@Override
+	public void handleEvent(SessionLoadedEvent e) {
+		// We have to read twice, first to set the nextID, then to assign ID to unassigned nodes
+		for(CyNetwork network : e.getLoadedSession().getNetworks()) {
+			CyTable nodeTable = network.getDefaultNodeTable();
+			
+			if(nodeTable.getColumn(NAMESPACE, COLUMN_NAME) == null) {
+				continue;
+			}
+			
+			for(CyRow nodeRow : nodeTable.getAllRows()) {
+				Long uuid = nodeRow.get(NAMESPACE, COLUMN_NAME, Long.class); 
+				if(uuid != null) {
+					uuid++;
+					nextUUID = (uuid > nextUUID ? uuid : nextUUID);
+				}
+			}
+		}
+		
+		// We assign IDs
+		for(CyNetwork network : e.getLoadedSession().getNetworks()) {
+			CyTable nodeTable = network.getDefaultNodeTable();
+			
+			if(nodeTable.getColumn(NAMESPACE, COLUMN_NAME) == null) {
+				nodeTable.createColumn(NAMESPACE, COLUMN_NAME, Long.class, true);
+			}
+			
+			for(CyRow nodeRow : nodeTable.getAllRows()) {
+				if(nodeRow.get(NAMESPACE, COLUMN_NAME, Long.class) == null) {
+					nodeRow.set(NAMESPACE, COLUMN_NAME, nextUUID++);
+				}
+			}
+		}
+	}
+
+}
